60069bf5d3b2dce903e5a093c3035c8608e7f5bb
Chutipong Roobklom <khunemz@gmail.com>
2020-04-04 00:46:00 +0700

    init

---
+++ ThaibevApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThaibevApp1
+//
+//  Created by ThaiBev on 4/4/20.
+//  Copyright © 2020 ThaiBev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
